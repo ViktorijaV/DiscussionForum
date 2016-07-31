18be3cd7b370f38d63fbbf8e961c1ff5997d51ae
frosinastojanovska <stojanovska.frose@gmail.com>
2016-07-31 23:03:28 +0200

    Add logic in User class

--- DiscussionForum/App_Code/User.cs
+++ DiscussionForum/App_Code/User.cs
@@ -1,11 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Text.RegularExpressions;
 
 namespace DiscussionForum.App_Code
 {
     public class User
     {
+        protected User()
+        {
+            DateCreated = DateTime.Now;
+            IsConfirmed = false;
+        }
+
+        public static User RegisterUser(string email, string fullName, string password, string repeatedPassword, Gender gender, DateTime birthday, string avatarUrl = "", string country = "", string faculty = "")
+        {
+            var user = new User();
+
+            if (!isValidEmail(email))
+                throw new Exception("Invalid email");
+
+            if (!isValidFullName(fullName))
+                throw new Exception("Invalid full name");
+
+            if (!IsValidPassword(password))
+                throw new Exception("Invalid password");
+
+            if (password != repeatedPassword)
+                throw new Exception("Passwords doesn't match");
+
+            user.Email = email;
+            user.UserName = email; // for now, username == email. Change later...
+            user.Password = password; // TODO: encrypt password
+            user.FullName = fullName;
+            user.RegistrationConfirmationCode = Guid.NewGuid().ToString();
+            user.Role = Role.User;
+            user.Gender = gender;
+            user.Birthday = birthday;
+            user.AvatarUrl = avatarUrl;
+            user.Country = country;
+            user.Faculty = faculty;
+
+            return user;
+        }
+
+        private static bool isValidEmail(string email)
+        {
+            const string VALID_EMAIL_REGEX = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+            if (Regex.IsMatch(email, VALID_EMAIL_REGEX))
+                return true;
+
+            return false;
+        }
+
+        private static bool isValidFullName(string fullName)
+        {
+            const string VALID_FULLNAME_REGEX = @"([A-Za-z -]+){6,}";
+            if (Regex.IsMatch(fullName, VALID_FULLNAME_REGEX))
+                return true;
+
+            return false;
+        }
+
+        private static bool IsValidPassword(string password)
+        {
+            if (Regex.IsMatch(password, @"((?=.*\w)(?=.*[!#$%&'()*+,-./:;<=>?@[\]^_`{|}~]).{6,})"))
+                return true;
+
+            return false;
+        }
+
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool IsConfirmed { get; set; }
+        public string RegistrationConfirmationCode { get; set; }
+        public Role Role { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime Birthday { get; set; }
+        public string AvatarUrl { get; set; }
+        public string Country { get; set; }
+        public string Faculty { get; set; }
+    }
+    public enum Role
+    {
+        User, Admin
+    }
+    public enum Gender
+    {
+        Male, Female
     }
 }
