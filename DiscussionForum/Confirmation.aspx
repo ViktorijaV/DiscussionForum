dd86e4b79bfad7abbbb4666c73fa44f6a04e689b
frosinastojanovska <stojanovska.frose@gmail.com>
2016-08-05 20:56:10 +0200

    Implement confirmation of User

--- DiscussionForum/Confirmation.aspx
+++ DiscussionForum/Confirmation.aspx
@@ -1,6 +1,9 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteMaster.Master" AutoEventWireup="true" CodeBehind="Confirmation.aspx.cs" Inherits="DiscussionForum.Confirmation" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h1>An email has been send to you. Please confirm your account.</h1>
+    <asp:Label ID="lblText" runat="server" Text="Label">
+        <h1>An email has been send to you. Please confirm your account.</h1>
+    </asp:Label>
 </asp:Content>
--- DiscussionForum/Confirmation.aspx.cs
+++ DiscussionForum/Confirmation.aspx.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Dapper;
+using DiscussionForum.App_Code;
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace DiscussionForum
 {
@@ -13,9 +13,27 @@ namespace DiscussionForum
         {
             if (!IsPostBack)
             {
-                if(Request.QueryString["code"] != null)
+                if (Request.QueryString["code"] != null)
                 {
+                    SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["myConnection"].ToString());
+                    using (connection)
+                    {
+                        connection.Open();
+                        string sql = $"SELECT * FROM Users WHERE ConfirmationCode='{Request.QueryString["code"]}'";
+                        var user = connection.Query<User>(sql).FirstOrDefault();
 
+                        if (user != null)
+                        {
+                            user.Confirmed = true;
+                            string updateQuery = $"UPDATE Users SET Confirmed = '{user.Confirmed}' WHERE ID='{user.ID}'";
+                            connection.Execute(updateQuery);
+                            lblText.Text = "<h1>Your account is confirmed!</h1>";
+                        }
+                        else
+                        {
+                            lblText.Text = "<h1>User with that confirmation link does not exists!</h1>";
+                        }
+                    }
                 }
             }
         }
