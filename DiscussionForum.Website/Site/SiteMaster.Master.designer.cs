b03e264cf9713c1ead0021b50249fd91b220a641
ViktorijaV <velinovska.viktorija@gmail.com>
2016-09-24 14:42:51 +0200

    Add dialog for invite user

--- DiscussionForum.Website/Site/SiteMaster.Master.designer.cs
+++ DiscussionForum.Website/Site/SiteMaster.Master.designer.cs
@@ -101,5 +101,14 @@ namespace DiscussionForum.Site {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// friendEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox friendEmail;
     }
 }
