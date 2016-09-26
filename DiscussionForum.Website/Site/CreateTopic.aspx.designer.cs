cfd988e8447741db0bcefcc7406cabc1b79c8d79
ViktorijaV <velinovska.viktorija@gmail.com>
2016-09-26 15:28:25 +0200

    Add dialog box for suggest category

--- DiscussionForum.Website/Site/CreateTopic.aspx.designer.cs
+++ DiscussionForum.Website/Site/CreateTopic.aspx.designer.cs
@@ -65,5 +65,23 @@ namespace DiscussionForum.Site {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnSubmit;
+        
+        /// <summary>
+        /// friendEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox friendEmail;
+        
+        /// <summary>
+        /// btnSave control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnSave;
     }
 }
